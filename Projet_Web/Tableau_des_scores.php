1e96b31ad507c2334740cc4f39c09bce559675e1
fkergus <64409062+fkergus@users.noreply.github.com>
2020-12-17 19:10:12 +0100

    Tableau_des_scores.php

--- Projet_Web/Tableau_des_scores.php
+++ Projet_Web/Tableau_des_scores.php
@@ -1,15 +1,15 @@
 <?php
 
-//connection a la BDD
+//Connexion à la BDD
 $link = mysqli_connect('localhost', 'root', 'root', 'geo_escape');
 
-//requete SQL
+//Requête SQL
 $requete="SELECT `Nom`,`Temps`,`Picture` FROM `joueurs`";
 
-//resultat de la requete
+//Résultat de la requête
 $results = mysqli_query($link,$requete);
 
-//si il y a un resultat on le renvoie en format json
+//S'il y a un résultat, on le renvoie en format json
 if ($results) {
     while ($name = mysqli_fetch_assoc($results)) {
         $nom[] = $name;
