b0e31962651d64cfda807d863929e10fcce554f6
fkergus <64409062+fkergus@users.noreply.github.com>
2020-12-17 19:12:46 +0100

    code.js

--- Projet_Web/code.js
+++ Projet_Web/code.js
@@ -3,17 +3,17 @@ var storage=window.localStorage
 //Si le jeu a été terminé
 if (storage.getItem(0)){
 
-  //on recupère les variables
+  //On récupère les variables
   var time_fin=storage.getItem(1);
   var text_fin=document.getElementById('fin_jeu');
-  //on ajoute un paragraphe à la page du debut
+  //On ajoute un paragraphe à la page de départ
   text_fin.innerHTML="Bravo, vous avez récupéré les clefs de la Goldie en "+time_fin+" ! <br>Vous pouvez rejouer si vous voulez,\
    mais nous savons tous les deux que ce sera moins marrant la deuxième fois !";
    storage.clear();
 }
 
 
-//fonction pour démarrer le jeu
+//Fonction pour démarrer le jeu
 function demarrer(){
   var pseudo=document.getElementById('nom').value
   if (pseudo.length==0){
