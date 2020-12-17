d857893d1c41540118482315a06c8e8334740646
fkergus <64409062+fkergus@users.noreply.github.com>
2020-12-17 19:05:03 +0100

    Tableau_Score.js

--- Projet_Web/Tableau_Score.js
+++ Projet_Web/Tableau_Score.js
@@ -1,17 +1,17 @@
 var Tableau=document.getElementById('classement')
 
-//on interroge la BDD pour avoir les scores
+//On interroge la BDD pour avoir les scores
 fetch('Tableau_des_scores.php?').then(function (result) {
     return result.json();
 
   }) .then(function (result) {
 
-    //on tri les scores par ordre croissant
+    //On trie les scores par ordre croissant
     var Scores=Tri_temps(result);
     
-    for(i=0; i<Scores.length;i++){ // on aprcour la liste des scores
+    for(i=0; i<Scores.length;i++){ //On parcourt la liste des scores
 
-      //on ajoute la photo, le nom et le temps dans le tableau
+      //On ajoute la photo, le nom et le temps dans le tableau
       Tableau.innerHTML+='<tr>\
       <td>'+(i+1)+'</td>\
       <td><img class=Icon_Score src="icons/Selfies/'+Scores[i]['Picture']+'" > </td>\
@@ -22,14 +22,14 @@ fetch('Tableau_des_scores.php?').then(function (result) {
 
     })
 
-//tri les résultat en fonction du temps réalisé
+//On trie les résultats en fonction du temps réalisé
 function Tri_temps(Liste){
   for(var i = 0; i < Liste.length; i++){
-    //stocker l'index de l'élément minimum
+    //On stocke l'index de l'élément minimum
     var min = i; 
     for(var j = i+1; j < Liste.length; j++){
       if(convert_heure(Liste[j]['Temps']) < convert_heure(Liste[min]['Temps'])){
-       // mettre à jour l'index de l'élément minimum
+       //On met à jour l'index de l'élément minimum
        min = j; 
       }
     }
@@ -40,7 +40,7 @@ function Tri_temps(Liste){
   return Liste;
 }
 
-//converti les minutes et les secondes et dixième de seconde
+//On convertit les minutes, secondes et dixièmes de seconde
 function convert_heure(heure){
   Units=heure.split(':');
   return(600*parseInt(Units[0])+10*parseInt(Units[1])+parseInt(Units[2]));
